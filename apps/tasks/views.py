dd6847809f8722ad7092dab3431a478be9397bbe
ugbereaseac <ugberaeseac@gmail.com>
2025-09-20 16:40:48 -0400

    feat: Class-based views and templates for tasks

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,1 +1,88 @@
-# views.py for the tasks app
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Task, Project
+from .forms import TaskForm
+from django.db.models import Q
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'tasks/list.html'
+    context_object_name = 'tasks'
+    paginate_by = 20
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(owner=self.request.user)
+
+        # Filtering
+        status = self.request.GET.get('status')
+        priority = self.request.GET.get('priority')
+        project_id = self.request.GET.get('project')
+
+        if status:
+            queryset = queryset.filter(status=status)
+        if priority:
+            queryset = queryset.filter(priority=priority)
+        if project_id:
+            queryset = queryset.filter(project__id=project_id)
+
+        # Searching
+        search_query = self.request.GET.get('q')
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) | Q(description__icontains=search_query)
+            )
+        return queryset.order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_status'] = self.request.GET.get('status', '')
+        context['current_priority'] = self.request.GET.get('priority', '')
+        context['current_project'] = self.request.GET.get('project', '')
+        context['search_query'] = self.request.GET.get('q', '')
+        context['projects'] = Project.objects.filter(owner=self.request.user)
+        return context
+
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Task
+    template_name = 'tasks/detail.html'
+    context_object_name = 'task'
+
+    def get_queryset(self):
+        return super().get_queryset().filter(owner=self.request.user)
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/form.html'
+    success_url = reverse_lazy('tasks:task_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/form.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks:task_list')
+
+    def get_queryset(self):
+        return super().get_queryset().filter(owner=self.request.user)
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/confirm_delete.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks:task_list')
+
+    def get_queryset(self):
+        return super().get_queryset().filter(owner=self.request.user)
